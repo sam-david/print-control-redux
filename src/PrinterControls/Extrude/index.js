2429f4049cd3ffbf6a0d278f80c00b17401f3710
Sam David <sfd1987@gmail.com>
2019-07-15 17:33:54 -0700

    prevent extrude below 180, prevent print control when printing

--- src/PrinterControls/Extrude/index.js
+++ src/PrinterControls/Extrude/index.js
@@ -8,134 +8,143 @@ import './style.scss';
 
 class Extrude extends Component {
   render() {
-    let extrudeControls;
-    return(
-      <div>
-        <Row className="extrude-button-row">
-          <Col s={2} className="extrude-button-column">
-            <Button
-                className="green extrude-button"
-                waves="light"
-                onClick={() => this.props.extrudeTool(this.props.selectedPrinter, 1)}
-              >
-               1 +
-            </Button>
-          </Col>
-          <Col s={2} className="extrude-button-column">
-            <Button
-                className="green extrude-button"
-                waves="light"
-                onClick={() => this.props.extrudeTool(this.props.selectedPrinter, 5)}
-              >
-               5 +
-            </Button>
-          </Col>
-          <Col s={2} className="extrude-button-column">
-            <Button
-                className="green extrude-button"
-                waves="light"
-                onClick={() => this.props.extrudeTool(this.props.selectedPrinter, 10)}
-              >
-               10 +
-            </Button>
-          </Col>
-          <Col s={2} className="extrude-button-column">
-            <Button
-                className="green extrude-button"
-                waves="light"
-                onClick={() => this.props.extrudeTool(this.props.selectedPrinter, 50)}
-              >
-               50 +
-            </Button>
-          </Col>
-          <Col s={2} className="extrude-button-column">
-            <Button
-                className="green extrude-button"
-                waves="light"
-                onClick={() => this.props.extrudeTool(this.props.selectedPrinter, 100)}
-              >
-               100 +
-            </Button>
-          </Col>
-          <Col s={2} className="extrude-button-column">
-            <Button
-                className="green extrude-button"
-                waves="light"
-                onClick={() => this.props.extrudeTool(this.props.selectedPrinter, 150)}
-              >
-               150 +
-            </Button>
-          </Col>
-        </Row>
-        <Row className="extrude-button-row">
-          <Col s={2} className="extrude-button-column">
-            <Button
-                className="red extrude-button"
-                waves="light"
-                onClick={() => this.props.extrudeTool(this.props.selectedPrinter, -1)}
-              >
-               1 -
-            </Button>
-          </Col>
-          <Col s={2} className="extrude-button-column">
-            <Button
-                className="red extrude-button"
-                waves="light"
-                onClick={() => this.props.extrudeTool(this.props.selectedPrinter, -5)}
-              >
-               5 -
-            </Button>
-          </Col>
-          <Col s={2} className="extrude-button-column">
-            <Button
-                className="red extrude-button"
-                waves="light"
-                onClick={() => this.props.extrudeTool(this.props.selectedPrinter, -10)}
-              >
-               10 -
-            </Button>
-          </Col>
-          <Col s={2} className="extrude-button-column">
-            <Button
-                className="red extrude-button"
-                waves="light"
-                onClick={() => this.props.extrudeTool(this.props.selectedPrinter, -50)}
-              >
-               50 -
-            </Button>
-          </Col>
-          <Col s={2} className="extrude-button-column">
-            <Button
-                className="red extrude-button"
-                waves="light"
-                onClick={() => this.props.extrudeTool(this.props.selectedPrinter, -100)}
-              >
-               100 -
-            </Button>
-          </Col>
-          <Col s={2} className="extrude-button-column">
-            <Button
-                className="red extrude-button"
-                waves="light"
-                onClick={() => this.props.extrudeTool(this.props.selectedPrinter, -150)}
-              >
-               150 -
-            </Button>
-          </Col>
-        </Row>
-      </div>
-    )
+    if (this.props.toolTemp > 180) {
+      return(
+        <div>
+          <Row className="extrude-button-row">
+            <Col s={2} className="extrude-button-column">
+              <Button
+                  className="green extrude-button"
+                  waves="light"
+                  onClick={() => this.props.extrudeTool(this.props.selectedPrinter, 1)}
+                >
+                 1 +
+              </Button>
+            </Col>
+            <Col s={2} className="extrude-button-column">
+              <Button
+                  className="green extrude-button"
+                  waves="light"
+                  onClick={() => this.props.extrudeTool(this.props.selectedPrinter, 5)}
+                >
+                 5 +
+              </Button>
+            </Col>
+            <Col s={2} className="extrude-button-column">
+              <Button
+                  className="green extrude-button"
+                  waves="light"
+                  onClick={() => this.props.extrudeTool(this.props.selectedPrinter, 10)}
+                >
+                 10 +
+              </Button>
+            </Col>
+            <Col s={2} className="extrude-button-column">
+              <Button
+                  className="green extrude-button"
+                  waves="light"
+                  onClick={() => this.props.extrudeTool(this.props.selectedPrinter, 50)}
+                >
+                 50 +
+              </Button>
+            </Col>
+            <Col s={2} className="extrude-button-column">
+              <Button
+                  className="green extrude-button"
+                  waves="light"
+                  onClick={() => this.props.extrudeTool(this.props.selectedPrinter, 100)}
+                >
+                 100 +
+              </Button>
+            </Col>
+            <Col s={2} className="extrude-button-column">
+              <Button
+                  className="green extrude-button"
+                  waves="light"
+                  onClick={() => this.props.extrudeTool(this.props.selectedPrinter, 150)}
+                >
+                 150 +
+              </Button>
+            </Col>
+          </Row>
+          <Row className="extrude-button-row">
+            <Col s={2} className="extrude-button-column">
+              <Button
+                  className="red extrude-button"
+                  waves="light"
+                  onClick={() => this.props.extrudeTool(this.props.selectedPrinter, -1)}
+                >
+                 1 -
+              </Button>
+            </Col>
+            <Col s={2} className="extrude-button-column">
+              <Button
+                  className="red extrude-button"
+                  waves="light"
+                  onClick={() => this.props.extrudeTool(this.props.selectedPrinter, -5)}
+                >
+                 5 -
+              </Button>
+            </Col>
+            <Col s={2} className="extrude-button-column">
+              <Button
+                  className="red extrude-button"
+                  waves="light"
+                  onClick={() => this.props.extrudeTool(this.props.selectedPrinter, -10)}
+                >
+                 10 -
+              </Button>
+            </Col>
+            <Col s={2} className="extrude-button-column">
+              <Button
+                  className="red extrude-button"
+                  waves="light"
+                  onClick={() => this.props.extrudeTool(this.props.selectedPrinter, -50)}
+                >
+                 50 -
+              </Button>
+            </Col>
+            <Col s={2} className="extrude-button-column">
+              <Button
+                  className="red extrude-button"
+                  waves="light"
+                  onClick={() => this.props.extrudeTool(this.props.selectedPrinter, -100)}
+                >
+                 100 -
+              </Button>
+            </Col>
+            <Col s={2} className="extrude-button-column">
+              <Button
+                  className="red extrude-button"
+                  waves="light"
+                  onClick={() => this.props.extrudeTool(this.props.selectedPrinter, -150)}
+                >
+                 150 -
+              </Button>
+            </Col>
+          </Row>
+        </div>
+      )
+    } else {
+      return(
+        <div />
+      )
+    }
   }
 }
 
 
 Extrude.propTypes = {
   jogPrinthead: PropTypes.func,
-  selectedPrinter: PropTypes.string
+  selectedPrinter: PropTypes.string,
+  printerStatus: PropTypes.string,
+  toolTemp: PropTypes.number
 }
 
 const mapStateToProps = state => ({
-  selectedPrinter: state.printers.selectedPrinter
+  selectedPrinter: state.printers.selectedPrinter,
+  toolTemp: state.printers.toolTemp,
+  printerStatus: state.printers.printerStatus
 })
 
 export default connect(mapStateToProps, { extrudeTool })(Extrude);
--- src/PrinterControls/index.js
+++ src/PrinterControls/index.js
@@ -11,31 +11,43 @@ import './style.scss';
 
 class PrinterControls extends Component {
   render() {
-    return(
-      <div>
-        <Row className="extrude-row">
-          <Col s={12} className="extrude-column">
-            <Extrude />
-          </Col>
-        </Row>
-        <Row>
-          <Col s={12} className="printer-control-column">
-            <Temp />
-          </Col>
-        </Row>
-        <Row>
-          <Col s={12}>
-            <Jog />
-          </Col>
-        </Row>
-
-      </div>
-    )
+    if (this.props.printerStatus != 'Printing') {
+      return(
+        <div>
+          <Row className="extrude-row">
+            <Col s={12} className="extrude-column">
+              <Extrude />
+            </Col>
+          </Row>
+          <Row>
+            <Col s={12} className="printer-control-column">
+              <Temp />
+            </Col>
+          </Row>
+          <Row>
+            <Col s={12}>
+              <Jog />
+            </Col>
+          </Row>
+        </div>
+      )
+    } else {
+      return(
+        <div>
+            <Row>
+              <Col s={12}>
+                <h1>Currently Printing</h1>
+              </Col>
+            </Row>
+        </div>
+      )
+    }
   }
 }
 
 const mapStateToProps = state => ({
-  selectedPage: state.nav.selectedPage
+  selectedPage: state.nav.selectedPage,
+  printerStatus: state.printers.printerStatus
 })
 
 export default connect(mapStateToProps, {})(PrinterControls);
