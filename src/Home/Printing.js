52cec6bd2ed7a47049d26b0c11b50d21057d87f4
Sam David <sfd1987@gmail.com>
2019-07-22 03:15:03 -0700

    add progress bar to printing screen

--- src/Home/Printing.js
+++ src/Home/Printing.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Row, Col, Button, Icon } from 'react-materialize';
+import { Row, Col, Button, ProgressBar } from 'react-materialize';
 import { connect } from 'react-redux';
 
 import { cancelPrintJob } from '../actions/printerActions';
 import Stream from '../Stream';
+import './printing.scss';
 
 class Printing extends Component {
   confirmAndCancelJob() {
@@ -15,12 +16,44 @@ class Printing extends Component {
     }
   }
 
+  parsePrintTime(time) {
+    let seconds = Math.floor(time % 60);
+    let hours = Math.floor((time / 60) / 60);
+    let minutes = Math.floor(time / 60);
+    let hourMinutes = Math.floor((time / 60) - (hours * 60))
+    if (hours > 0) {
+      return hours + " hours " + hourMinutes + " min. ";
+    } else {
+      return minutes + " min.";
+    }
+    // return Moment("2015-01-01").startOf('day')
+    // .seconds(time)
+    // .format('H:mm:ss');
+  }
+
   render() {
     return(
       <div className="cancel-container">
         <Row>
           <Col s={12}>
             <h1>Printing...</h1>
+          </Col>
+        </Row>
+        <Row className="progress-bar-row">
+          <Col s={12} className="progress-bar-column">
+            <ProgressBar className={this.props.selectedPrinter} progress={this.props.printProgress} />
+          </Col>
+        </Row>
+        <Row>
+          <Col s={12} className="progress-percent-column">
+            <p className="progress-precent">
+            { Math.round(this.props.printProgress, 2) }%
+            </p>
+          </Col>
+        </Row>
+        <Row className="progress-bar-row">
+          <Col s={12} className="print-time">
+            { this.parsePrintTime(this.props.printTimeLeft) }
           </Col>
         </Row>
         <Row>
@@ -43,11 +76,15 @@ class Printing extends Component {
 
 Printing.propTypes = {
   cancelPrintJob: PropTypes.func,
-  selectedPrinter: PropTypes.string
+  selectedPrinter: PropTypes.string,
+  printTimeLeft: PropTypes.number,
+  printProgress: PropTypes.number
 }
 
 const mapStateToProps = state => ({
-  selectedPrinter: state.printers.selectedPrinter
+  selectedPrinter: state.printers.selectedPrinter,
+  printTimeLeft: state.printers.printTimeLeft,
+  printProgress: state.printers.printProgress
 })
 
 export default connect(mapStateToProps, {
